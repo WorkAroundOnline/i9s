1211dfac17c7976ce5b455df6c602a52a7c15276
Severin Ibarluzea <seveibar@gmail.com>
2020-05-08 15:18:04 -0400

    Update test.js

--- create-react-app/challenge1/test.js
+++ create-react-app/challenge1/test.js
@@ -9,10 +9,10 @@ test("top function should return top 2 numbers over average for [1,2,3,4,5]", t 
   t.deepEqual(top([1, 2, 3, 4, 5]), [4, 5])
 })
 
-test("bottom function should return bottom 2 numbers over average for [1,2,4,5]", t => {
+test("bottom function should return bottom 2 numbers below average for [1,2,4,5]", t => {
   t.deepEqual(bottom([1, 2, 4, 5]), [1, 2])
 })
 
-test("bottom function should return bottom 2 numbers over average for [1,2,3,4,5]", t => {
+test("bottom function should return bottom 2 numbers below average for [1,2,3,4,5]", t => {
   t.deepEqual(bottom([1, 2, 3, 4, 5]), [1, 2])
 })
